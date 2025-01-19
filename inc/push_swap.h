56762b6ca21c578179a73999e573da297f82634f
KameCode42 <votre.email@example.com>
2025-01-19 15:03:29 +0100

    add makefile

---
+++ inc/push_swap.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/19 13:50:35 by david             #+#    #+#             */
+/*   Updated: 2025/01/19 13:52:26 by david            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+# include "../libraries/libft/libft.h"
+
+#endif

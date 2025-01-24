587cea21e5e93499b845f8e6824485eb67f7e3ec
KameCode42 <votre.email@example.com>
2025-01-24 18:56:57 +0100

    add comm

--- src/push.c
+++ src/push.c
@@ -6,7 +6,7 @@
 /*   By: david <david@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 12:31:08 by dle-fur           #+#    #+#             */
-/*   Updated: 2025/01/24 18:37:05 by david            ###   ########.fr       */
+/*   Updated: 2025/01/24 18:56:01 by david            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ void	push_a(t_stacks *stack)
 	i = stack->size_a;
 	while (i > 0)
 	{
-		stack->a[i] = stack->a[i - 1];
+		stack->a[i] = stack->a[i - 1]; //decale la pile a pour faire de la place
 		i--;
 	}
 	stack->a[0] = tmp;
 	i = 0;
 	while (i < stack->size_b - 1)
 	{
-		stack->b[i] = stack->b[i + 1];
+		stack->b[i] = stack->b[i + 1]; //decale les nbr vers le haut
 		i++;
 	}
 	stack->size_b--;
@@ -47,14 +47,14 @@ void	push_b(t_stacks *stack)
 	i = stack->size_b;
 	while (i > 0)
 	{
-		stack->b[i] = stack->b[i - 1];
+		stack->b[i] = stack->b[i - 1]; //decale la pile b pour faire de la place
 		i--;
 	}
 	stack->b[0] = tmp;
 	i = 0;
 	while (i < stack->size_a - 1)
 	{
-		stack->a[i] = stack->a[i + 1];
+		stack->a[i] = stack->a[i + 1]; // decale les nbr vers le haut
 		i++;
 	}
 	stack->size_a--;
